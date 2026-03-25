36041ce0b9132d8ed154f6920af5fcec5ff7de29
Mateus Pereira Alvim <142549988+Astutez@users.noreply.github.com>
2026-03-25 23:35:55 +0000

    Criação do Primeiro Projeto Dart

---
+++ primeiro_projeto_parcial/test/primeiro_projeto_parcial_test.dart
@@ -0,0 +1,8 @@
+import 'package:primeiro_projeto_parcial/primeiro_projeto_parcial.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
