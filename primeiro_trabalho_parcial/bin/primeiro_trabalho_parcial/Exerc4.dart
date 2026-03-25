2e83c50153f208d6566d651c3ccde70420943421
Mateus Pereira Alvim <142549988+Astutez@users.noreply.github.com>
2026-03-25 20:30:50 -0300

    Delete primeiro_trabalho_parcial directory

--- primeiro_trabalho_parcial/bin/primeiro_trabalho_parcial/Exerc4.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-void main () {
-  stdout.write("\n --- Calculadora de Rendimentos Após um Mês --- \n");
-    stdout.write("\nInsira o valor do depósito: ");
-    double? deposito = double.tryParse(stdin.readLineSync()!);
-    
-    double rendimentos =  deposito! * 0.005; 
-    stdout.write("\nO valor do rendimento foi de: $rendimentos e o total foi: ${rendimentos + deposito}\n");
-}
